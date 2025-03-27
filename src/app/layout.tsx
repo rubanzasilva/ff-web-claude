d33e8453ce152cf45b06bc2676b5352cf594ca26
rubanzasilva <rubanza@rubanzasilver.xyz>
2025-03-27 11:06:06 +0300

    adding vercel web analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 // src/app/layout.tsx
 
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
